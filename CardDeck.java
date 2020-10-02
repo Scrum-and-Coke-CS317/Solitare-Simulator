132ed2a00c8e318ab84567f2ffa596a0dba587c8
JTBrakob <justin.brakob@gmail.com>
2020-10-02 14:58:33 -0500

    Create CardDeck.java

---
+++ CardDeck.java
@@ -0,0 +1,36 @@
+/**
+ * card deck class
+ * 
+ * @author scrum-and-coke
+ */
+//use a queue or something??
+
+public class CardDeck 
+{
+	private int remainingCards;
+	
+	public void shuffle()
+	{
+		
+	}
+	
+	public void draw()
+	{
+		
+	}
+	
+	public int getRemainingCards()
+	{
+		return remainingCards;
+	}
+	
+	public void discard(Card c)
+	{
+		//send to back
+	}
+	
+	public void initialize()
+	{
+		
+	}
+}
