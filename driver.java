b3b85b25d56db06c9fbf19816c245091ceb846b4
JTBrakob <44072519+JTBrakob@users.noreply.github.com>
2020-10-04 13:43:24 -0500

    Update driver.java
    better format

--- driver.java
+++ driver.java
@@ -1,8 +1,9 @@
-
-public class driver {
- public static void main(String[] args) {
+public class driver 
+{
+ public static void main(String[] args) 
+ {
 	CardDeck carddeck = new CardDeck();
 	carddeck.initialize();
 	carddeck.printDeck();
-}
+ }
 }
