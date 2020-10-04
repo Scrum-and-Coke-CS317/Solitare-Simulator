3b7abf6cbed24026e3686b690d7ae2474adbda77
Mason Heimer <61022271+BabySharkByte@users.noreply.github.com>
2020-10-04 18:00:59 -0500

    MHeimer: complete CardDeck Test Class

--- CardDeckTest.java
+++ CardDeckTest.java
@@ -1,47 +1,68 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CardDeckTest {
-
+	CardDeck testDeck;
+	Card testCard;
+	
 	@BeforeEach
 	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+		testDeck = new CardDeck();
+		testDeck.initialize();
 	}
 
 	@Test
 	void testDraw() {
-		fail("Not yet implemented");
+		for (int i = 0 ; i < 52 ; i++) {
+			assertTrue(testDeck.getDeckSize() == 52-i);	
+			testCard = testDeck.draw();
+			assertTrue( ( testCard.getValue() == 1 || testCard.getValue() == 2 || testCard.getValue() == 3 || 
+					testCard.getValue() == 4 || testCard.getValue() == 5 || testCard.getValue() == 6 || testCard.getValue() == 7 ||
+					 testCard.getValue() == 8 || testCard.getValue() == 9 || testCard.getValue() == 10 || testCard.getValue() == 11 ||
+					 testCard.getValue() == 12 || testCard.getValue() == 13) );
+			assertTrue( ( testCard.getSuit() == "spade" || testCard.getSuit() == "club" || testCard.getSuit() == "heart" || 
+					testCard.getSuit() == "diamond" ) );
+		}
 	}
 
 	@Test
 	void testGetRemainingCards() {
-		fail("Not yet implemented");
+		//doesn't contain drawn cards
+		testCard = testDeck.draw();
+		assertFalse(testDeck.getRemainingCards().contains(testCard));
+		//doesn't contain null
+		assertFalse(testDeck.getRemainingCards().contains(null));
 	}
 
 	@Test
 	void testDiscard() {
-		fail("Not yet implemented");
+		//discarded card goes back into deck
+		testCard = testDeck.draw();
+		assertFalse(testDeck.getRemainingCards().contains(testCard));
+		testDeck.discard(testCard);
+		assertTrue(testDeck.getRemainingCards().contains(testCard));
 	}
 
 	@Test
 	void testGetDeckSize() {
-		fail("Not yet implemented");
+		assertTrue(testDeck.getDeckSize() == 52);	
+		testCard = testDeck.draw();
+		assertTrue(testDeck.getDeckSize() == 51);	
 	}
 
 	@Test
 	void testInitialize() {
-		fail("Not yet implemented");
+		testDeck.initialize();
+		assertTrue(testDeck.getDeckSize() == 52);	
+		testDeck.initialize();
+		assertTrue(testDeck.getDeckSize() == 52);	
 	}
 
 	@Test
 	void testPrintDeck() {
-		fail("Not yet implemented");
+		//n/a
 	}
 
 }
